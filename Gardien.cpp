a019be8ebb4e68cc5dd91dd5c57bebcc0b7acb2f
Stephane Romany <sromany@a-018259.ups.u-psud.fr>
2017-05-04 18:40:28 +0200

    First Commit du jour : Pret à l'emploi

---
+++ Gardien.cpp
@@ -0,0 +1,191 @@
+//#s
+#include <vector>
+#include <utility>  
+#include <cmath>
+#include <random>
+//#e
+#include "Gardien.h"
+//#s
+#include "Labyrinthe.h" 
+using namespace std;
+//#e
+//Constructor
+Gardien::Gardien(Labyrinthe* l, int x, int y, const char* modele) : Mover (x, y, l, modele)
+{
+	_guard_fire = new Sound ("sons/guard_fire.wav");
+	_guard_hit = new Sound ("sons/guard_hit.wav");
+	//~ if (_wall_hit == 0)
+		//~ _wall_hit = new Sound ("sons/hit_wall.wav");
+	fm.hit = false;
+	fm.trigger = false;
+}
+
+
+// Le gardien pense ici
+void Gardien::update(void) {
+ /*
+
+    if (this == this->_l->_guards[1]) {
+        this->_l->_totalPotenciel = 0;
+        this->_l->calcDistance();
+    }
+
+    potentiel();
+
+    this->_l->_totalPotentiel += _potentiel;
+
+    if (_potentiel < this->_l->_seuil) {
+        // _status = defense
+    }
+
+    // tracking
+
+    // look chasseur
+*/	
+	// Ici si le gardien nous voit : fire && wait !
+	// if(seeHunter && targetLock) = seekHunter : faire deux fonctions : champ de vision et turn for fire
+	_angle = 180;
+	if(fm.trigger == false)
+	{ 
+		fire(0);		
+		fm.trigger = true;
+		fm.hit = false;
+	}else{
+		int delay = 1000 * (fd.stop - fd.start)/CLOCKS_PER_SEC;
+		if(fm.hit){
+			if(delay >= 1000){
+				fm.trigger = false;
+			}else{				
+				fd.stop = clock();									
+			}
+		}
+	}
+	
+}
+
+/*
+// update pontetiel
+void Gardien::potentiel() {
+    _potentiel = this->_l->distance(_x, _y) / this->_l->_totalDistance;
+}
+*/
+
+// et ne bouge pas!
+//#s
+bool Gardien::move (double dx, double dy) { 
+
+	int x = (int)((_x + dx) / Environnement::scale);
+	int y = (int)((_y + dy) / Environnement::scale);
+
+	Labyrinthe* l = (Labyrinthe*) _l;
+
+	if (l->isAccessible(x, y)) {
+		
+		_x += dx;
+		_y += dy;
+
+		return true;		
+
+	}
+
+	return false;
+
+}
+//#e
+
+// ne sait pas tirer sur un ennemi.
+void Gardien::fire (int angle_vertical) {
+    // _guard_fire -> play ();
+	_fb -> init (_x, _y, 10.,          /* position initiale de la boule xyz */ 
+				 angle_vertical, _angle); /* angles de visée vertical - horizontal */ 
+	fd.start = clock();
+}
+
+
+// quand a faire bouger la boule de feu...
+bool Gardien::process_fireball (float dx, float dy)
+{
+	// calculer la distance entre le chasseur et le lieu de l'explosion.
+	float	x = (_x - _fb -> get_x ()) / Environnement::scale;
+	float	y = (_y - _fb -> get_y ()) / Environnement::scale;
+	float	dist2 = x*x + y*y;
+	// on bouge que dans le vide !
+	if (EMPTY == _l -> data ((int)((_fb -> get_x () + dx) / Environnement::scale),
+							 (int)((_fb -> get_y () + dy) / Environnement::scale)))
+	{
+		//~ message ("Woooshh ..... %d", (int) dist2);
+		// il y a la place.
+		return true;
+	}
+	fd.stop = clock();
+	// collision...
+	// calculer la distance maximum en ligne droite.
+	float	dmax2 = (_l -> width ())*(_l -> width ()) + (_l -> height ())*(_l -> height ());
+	// faire exploser la boule de feu avec un bruit fonction de la distance.
+	//~ _wall_hit -> play (1. - dist2/dmax2);
+	fm.hit = true;
+	//~ message ("Booom...");
+	return false;
+}
+
+
+void Gardien::seekHunter(){
+	
+	
+}
+
+bool Gardien::targetHunter(){
+	return true;
+}
+//#s
+void Gardien::setAngle(int x, int y) {
+
+	//https://en.wikipedia.org/wiki/Octant_(solid_geometry)
+	vector<pair<int, pair<int, int>>> octants;
+	octants.push_back(make_pair(1, make_pair(  0,  1)));
+	octants.push_back(make_pair(2, make_pair( -1,  1)));
+	octants.push_back(make_pair(3, make_pair( -1,  0)));
+	octants.push_back(make_pair(4, make_pair( -1, -1)));
+	octants.push_back(make_pair(5, make_pair(  0, -1)));
+	octants.push_back(make_pair(6, make_pair(  1, -1)));
+	octants.push_back(make_pair(7, make_pair(  1,  0)));
+	octants.push_back(make_pair(8, make_pair(  1,  1)));
+	//http://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
+	//
+	random_device rd; 
+	//
+	mt19937 rng(rd());
+	//	
+	while (!octants.empty()){
+
+		//
+		uniform_int_distribution<> cell(0, octants.size()-1);
+		//
+		int index = cell(rng);	
+		//
+		int octant = octants[index].first;
+		//
+		int i = octants[index].second.first;
+		int j = octants[index].second.second;
+		//
+		Labyrinthe* l = (Labyrinthe*) _l;
+		//
+		if (l->isAccessible(x + i, y + j)) {
+			//
+			uniform_int_distribution<> angle(((octant - 1) * 45) - 22.5, (octant * 45) - 22.5);
+			//
+			_angle = angle(rng);
+			//
+			if(_angle < 0) {
+				_angle += 360;		
+			}
+			//
+			break;
+		} else {
+			//
+			octants.erase(octants.begin() + index);
+		}
+	}
+
+}
+//#e
---
+++ Gardien.h
@@ -0,0 +1,54 @@
+#ifndef GARDIEN_H
+#define GARDIEN_H
+
+#include <stdio.h>
+#include <time.h>
+#include "Mover.h"
+#include "Sound.h"
+
+// Déclencheurs pour l'action fire des gardiens 
+typedef struct{
+	bool hit, trigger;	
+}FireMechanics;
+
+// Intervale de temps entre les tirs des gardiens 
+typedef struct{
+	clock_t start, stop;
+}Delay;
+
+class Labyrinthe;
+
+class Gardien : public Mover {
+	//#s	
+	private:
+		void setAngle(int x, int y);
+	//#e
+	public:
+		FireMechanics fm;
+		Delay fd;
+		/*
+		 *	Le son...
+		 */
+		static Sound*	_guard_fire;	// bruit de l'arme du gardien.
+		static Sound*	_guard_hit;	    // cri du gardien touché.
+		static Sound*	_wall_hit;		// on a tapé un mur.
+
+
+		Gardien (Labyrinthe* l, int x, int y, const char* modele);
+
+		// mon gardien pense très mal!
+		void update (void);
+
+		void potentiel();
+		void seekHunter();
+		bool targetHunter();
+
+		// et ne bouge pas!
+		bool move (double dx, double dy);
+		// ne sait pas tirer sur un ennemi.
+		void fire (int angle_vertical);
+		// quand a faire bouger la boule de feu...
+		bool process_fireball (float dx, float dy);
+};
+
+#endif
