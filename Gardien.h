5eb2e7d385e4f0a8ae1d08557721fef7892ef165
Anderson Carlos Ferreira da Silva <andersoncarlosfs@outlook.com>
2017-05-05 16:43:15 +0200

    Update Gardien.h

--- Gardien.h
+++ Gardien.h
@@ -33,7 +33,11 @@ class Gardien : public Mover {
 		static Sound*	_guard_hit;	    // cri du gardien touché.
 		static Sound*	_wall_hit;		// on a tapé un mur.
 		int _pv;
-		
+		//#s
+                static int _distance_max;
+                static float _potentiel_max;
+                float _potentiel;
+		//#e
 	public:
 		Gardien (Labyrinthe* l, int x, int y, const char* modele);
 
